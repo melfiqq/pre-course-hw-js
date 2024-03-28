8c42d4a54ede90698ef69351305a638b11b231aa
Maryie <mashenka.zinkevich0245@gmail.com>
2024-03-28 22:41:07 +0200

    Deleted all except 02.js

--- lesson-01/06.js
+++
@@ -1,3 +0,0 @@
-let str="zzzzyzz";
-
-str.includes("y") ? console.log("нашел") : console.log(заблудился);
--- lesson-01/07.js
+++
@@ -1,7 +0,0 @@
-let iAm = 'я';
-let krasavchik = 'красавчик!';
-
-//console.log(iAm.toUpperCase, krasavchik);
-//console.log(iAm[0].toUpperCase, krasavchik);
-
-console.log(iAm.toUpperCase(), krasavchik);
