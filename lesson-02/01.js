b08dfa76c4e2ebbedb0ffa43022e14c6a119f57b
Maryie <mashenka.zinkevich0245@gmail.com>
2024-03-28 19:32:58 +0200

    Changed the code by creating a new pice of code

--- lesson-02/01.js
+++ lesson-02/01.js
@@ -1,1 +1,8 @@
-// code
+
+let number1 = Math.floor(Math.random() * 100);
+const even = ":Это четное число";
+const odd = ":Это нечетное число";
+number1 = number1 % 2 === 0 ? [number1] + even : [number1] + odd;
+
+console.log(number1);
+
