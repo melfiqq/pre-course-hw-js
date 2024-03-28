949fe62d06853d051284de97eaf8fca87d5fc00f
Maryie <mashenka.zinkevich0245@gmail.com>
2024-03-28 23:00:19 +0200

    changed to check.

--- lesson-01/03.js
+++ lesson-01/03.js
@@ -1,9 +1,6 @@
-let number = 1;
+let number = Math.floor(Math.random() * 100);
+const even = ":Это четное 00000число";
+const odd = ":Это нечетное 00000число";
+number1 = number1 % 2 === 0 ? [number1] + even : [number1] + odd;
 
-console.log(number++);
-
-console.log(++number);
-
-console.log(number--);
-
-console.log(--number);
+console.log(number1);
