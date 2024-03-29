97fbae455f836b5382c24a484d4b5f34ea7af505
Maryie <mashenka.zinkevich0245@gmail.com>
2024-03-29 13:10:29 +0200

    Fixed 05.js

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -9,8 +9,9 @@ case rule.length < 25:
 case rule.length > 25:
     console.log("У меня 100 пудов все получится");
     break;
-    
+
 case rule.length = 25:
     console.log("50 на 50");
-    break;   
+    break;
+
 }
