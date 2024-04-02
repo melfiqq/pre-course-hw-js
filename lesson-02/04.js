6dcfd2ec9b40b09915184c6cd4faa2e9a0a1464a
Maryie <mashenka.zinkevich0245@gmail.com>
2024-04-02 20:46:46 +0300

    Update for 04.js

--- lesson-02/04.js
+++ lesson-02/04.js
@@ -1,8 +1,9 @@
-const giveMeRightNumbers = (value1, value2, maximum) => {
+const giveMeRightCondition = (value1, value2, maximum) => {
     if (value1 === value2 && value1 <= maximum) {
         console.log('Success!');
     } else {
         console.log('Something wrong!');
     }
 }
-giveMeRightNumbers(201, 100, 200)
+
+giveMeRightCondition(100, 100, 200)
