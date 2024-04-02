aa5aab0c7829042dfed4f0fe08a9c36188adaa22
Maryie <mashenka.zinkevich0245@gmail.com>
2024-04-02 21:53:57 +0300

    05.js update

---
+++ lesson-02/05.js
@@ -0,0 +1,11 @@
+let passport = {
+    name: "Petr",
+    surname: "Petrov",
+};
+
+let passportCopy = Object.assign({}, passport);
+
+passportCopy.name = "Ivan";
+
+console.log(passport.name);
+console.log(passportCopy.name);
